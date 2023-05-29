30cf64a1b47f60d3001f402bb7cc3d6a61bcff74
ADRIEL BARBOSA BORGES <1091392123030@fatec-srv.local>
2023-05-29 20:05:00 -0300

    Use Location

--- android/app/src/hooks/useLocation.js
+++ android/app/src/hooks/useLocation.js
@@ -17,9 +17,35 @@ export default () => {
                     (statuses) => {
                         const statusFine = statuses[PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION];
                         const statusBack = statuses[PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION];
+
+                        if (Platform.Version < 29) {
+                            if (statusFine == 'granted') {
+                                return true
+                            } else {
+                                setErrorMsg('Usuário não aceitou solicitação de uso do GPS')
+                            }
+                        }
+                        if (statusFine == 'granted' && statusBack == 'granted') {
+                            return true;
+                        } else {
+                            setErrorMsg('Usuário não aceitou solicitação de uso do GPS')
+                        }
                     }
                 )
+                if(result){
+                    await Geolocation.getCurrentPosition(
+                        ({coords}) => {
+                            setCoords({
+                                latitude: coords.latitude,
+                                longitude: coords.longitude
+                            })                        
+                        }, (error) => {
+                            setErrorMsg('Não foi possível obter a localização!')
+                        }, { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, showLocationDialog: true }
+                    )
+                }
         }
         )();
     }, []);
+    return{coords, errorMsg};
 }
